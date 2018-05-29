e891c1741fc8d4d85800b3b9ee2c3c08ca98350f
Jaime Machuca <jaime@element.aero>
2018-05-29 17:13:12 -0500

    Fixed commit for Init_wifi.py

--- tools/init_wifi.py
+++ tools/init_wifi.py
@@ -1,58 +1,48 @@
-<<<<<<< HEAD:scripts/init_wifi.py
-# wificontrol code is placed under the GPL license.
-# Written by Ivan Sapozhkov (ivan.sapozhkov@emlid.com)
-# Copyright (c) 2016, Emlid Limited
-=======
 #!/usr/bin/python
 
 # Written by Aleksandr Aleksandrov <aleksandr.aleksandrov@emlid.com>
 #
 # Copyright (c) 2017, Emlid Limited
->>>>>>> 9f8637d... init_wifi: init example moved to tools, updated to a newer version:tools/init_wifi.py
 # All rights reserved.
-
-# If you are interested in using wificontrol code as a part of a
-# closed source project, please contact Emlid Limited (info@emlid.com).
-
-# This file is part of wificontrol.
-
-# wificontrol is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# wificontrol is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with wificontrol.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Redistribution and use in source and binary forms,
+# with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 import sys
 
 from wificontrol import WiFiControl
 
-<<<<<<< HEAD:scripts/init_wifi.py
-def connection_callback(result, wific):
-    if not result:
-        print("Can't connect to any network.")
-        print("Start HOSTAP mode")
-        try:
-            wific.start_host_mode()
-        except WiFiControlError as error:
-            print(error)
-            sys.exit(2)
-        else: 
-            print("In HOST mode")
-            sys.exit(10)
-=======
 
 def _show_result(result, wifi_controller):
     if result:
         sys.stdout.write("Network mode: client")
->>>>>>> 9f8637d... init_wifi: init example moved to tools, updated to a newer version:tools/init_wifi.py
     else:
         if wifi_controller.start_host_mode():
             sys.stdout.write("Network mode: master")
